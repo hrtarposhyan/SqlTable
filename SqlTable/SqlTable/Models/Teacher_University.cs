3ffd83de4f31a6f6dbf40244bd01970fc7f2611d
hrtarposhyan <hrtarposhyan@gmail.com>
2020-02-28 05:42:50 -0800

    Add Teacher_University relation

---
+++ SqlTable/SqlTable/Models/Teacher_University.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlTable.Models
+{
+    class Teacher_University
+    {
+        public int Id { get; set; }
+        public int TeacherId { get; set; }
+        public int UniversityId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Hour { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}\t{TeacherId}\t{UniversityId}\t{StartDate}\t{EndDate}\t{Hour}";
+        }
+    }
+}
